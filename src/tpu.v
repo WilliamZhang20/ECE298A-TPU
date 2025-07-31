46063c4d7135e941f132081f54b4b9d131b46dd5
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-07-31 11:58:42 -0400

    double up buffering

--- src/tpu.v
+++ src/tpu.v
@@ -40,6 +40,9 @@ module tt_um_tpu (
     wire [7:0] stage1;
     wire [7:0] stage2;
     wire [7:0] stage3;
+    wire [7:0] stage4;
+    wire [7:0] stage5;
+    wire [7:0] stage6;
 
     // Module Instantiations
     memory mem (
@@ -104,9 +107,12 @@ module tt_um_tpu (
             (* keep *) buffer buf1 (.A(out_data[i]), .X(stage1[i]));
             (* keep *) buffer buf2 (.A(stage1[i]), .X(stage2[i]));
             (* keep *) buffer buf3 (.A(stage2[i]), .X(stage3[i]));
+            (* keep *) buffer buf4 (.A(stage3[i]), .X(stage4[i]));
+            (* keep *) buffer buf5 (.A(stage4[i]), .X(stage5[i]));
+            (* keep *) buffer buf6 (.A(stage5[i]), .X(stage6[i]));
         end
     endgenerate
-    assign uo_out = stage3;
+    assign uo_out = stage6;
     assign uio_out = {done, state, 5'b0};
     assign uio_oe = 8'b11100000;
 
