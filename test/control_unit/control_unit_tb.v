ca43b6775668e73813f0ae8cbfbd7d629fa65a43
William Zhang <wzhang20@yahoo.com>
2025-08-05 14:12:13 -0400

    fix ctrl unit test error

--- test/control_unit/control_unit_tb.v
+++ test/control_unit/control_unit_tb.v
@@ -18,7 +18,6 @@ module control_unit_tb ();
     reg clk;
     reg rst;
     reg load_en;
-    reg[1:0] state_out;
 
     // Outputs
     reg [2:0] mem_addr;
@@ -31,8 +30,7 @@ module control_unit_tb ();
         .load_en(load_en),
         .mem_addr(mem_addr),
         .mmu_en(mmu_en),
-        .mmu_cycle(mmu_cycle),
-        .state_out(state_out)
+        .mmu_cycle(mmu_cycle)
     );
 
 endmodule
