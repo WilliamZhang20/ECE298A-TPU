5679eba669d4463c7a59697a63350fbf308c6003
William Zhang <wzhang20@yahoo.com>
2025-08-05 14:19:35 -0400

    buffer more, still 1.63

--- src/tpu.v
+++ src/tpu.v
@@ -39,6 +39,8 @@ module tt_um_tpu (
     wire [7:0] stage1;
     wire [7:0] stage2;
     wire [7:0] stage3;
+    wire [7:0] stage4;
+    wire [7:0] stage5;
 
     // Module Instantiations
     memory mem (
@@ -102,10 +104,12 @@ module tt_um_tpu (
             (* keep *) buffer buf1 (.A(out_data[i]), .X(stage1[i]));
             (* keep *) buffer buf2 (.A(stage1[i]), .X(stage2[i]));
             (* keep *) buffer buf3 (.A(stage2[i]), .X(stage3[i]));
+            (* keep *) buffer buf4 (.A(stage3[i]), .X(stage4[i]));
+            (* keep *) buffer buf5 (.A(stage4[i]), .X(stage5[i]));
         end
     endgenerate
     
-    assign uo_out = stage3;
+    assign uo_out = stage5;
     assign uio_out = {done, 7'b0};
     assign uio_oe = 8'b11100000;
 
